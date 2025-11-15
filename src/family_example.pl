6eac786472d6f80952d932fcb45451569dc9d81b
Akmal2205 <syarafiakml2004@gmail.com>
2025-11-16 00:01:56 +0700

    add: bokap, nyokap

--- src/family_example.pl
+++ src/family_example.pl
@@ -1,7 +1,14 @@
 /* family tree */
-person(ahmed, male).
-person(fatima, female).
+person(ahmed, male, alive).
+person(fatima, female, alive).
+person(hamzah, male, alive).
+person(khadijah, female, alive).
+person(khalid, male, alive).
+person(amina, female, deceased).
+
 spouse(ahmed, fatima).
+spouse(hamzah, khadijah).
 
 parent_child(ahmed, khalid, both).
 parent_child(ahmed, amina, both).
+parent_child(hamzah, ahmed, both).
--- src/hukum_waris.pl
+++ src/hukum_waris.pl
@@ -3,34 +3,74 @@ consultFiles:-
 
 /* RULES */
 deceased_has_children(D, Result):-
-    (parent_child(D, _, _) -> Result = true ; Result = false).
+    ((parent_child(D, Child, _), person(Child, _, alive))
+    ->  Result = true ; Result = false
+    ).
 
+/* SPOUSES */
 /* husband inherits from wife */
 /* has no children */
 inherit_from(Deceased, Husband, Share):-
-    person(Husband, male),
+    person(Husband, male, alive),
     spouse(Husband, Deceased),
-    deceased_has_children(Deceased, false),
-    Share is 1/2.
+    deceased_has_children(Husband, false),
+    Share = '1/2'.
 
 /* has children */
 inherit_from(Deceased, Husband, Share):-
-    person(Husband, male),
+    person(Husband, male, alive),
     spouse(Husband, Deceased),
-    deceased_has_children(Deceased, true),
-    Share is 1/4.
+    deceased_has_children(Husband, true),
+    Share = '1/4'.
 
 /* wife inherits from husband */
 /* has no children */
 inherit_from(Deceased, Wife, Share):-
-    person(Wife, female),
+    person(Wife, female, alive),
     spouse(Deceased, Wife),
     deceased_has_children(Deceased, false),
-    Share is 1/4.
+    Share = '1/4'.
 
 /* has children */
 inherit_from(Deceased, Wife, Share):-
-    person(Wife, female),
+    person(Wife, female, alive),
     spouse(Deceased, Wife),
     deceased_has_children(Deceased, true),
-    Share is 1/8.
+    Share = '1/8'.
+
+/* ASCENDANTS */
+/* father inherits from deceased */
+/* has no children */
+inherit_from(Deceased, Father, Share):-
+    person(Father, male, alive),
+    parent_child(Father, Deceased, _),
+    deceased_has_children(Deceased, false),
+    spouse(Father, Mother),
+    (person(Mother, female, alive) -> Share = '2/3 * T' ; Share = 'T').
+
+/* has children */
+inherit_from(Deceased, Father, Share):-
+    person(Father, male, alive),
+    parent_child(Father, Deceased, _),
+    deceased_has_children(Deceased, true),
+    (person(Child, male, alive), parent_child(Deceased, Child, _) -> Share = '1/6' ; Share = '1/6 + T').
+
+/* mother inherits from deceased */
+/* has no children */
+inherit_from(Deceased, Mother, Share):-
+    person(Mother, female, alive),
+    spouse(Father, Mother),
+    parent_child(Father, Deceased, _),
+    deceased_has_children(Deceased, false),
+    (person(Father, male, alive) -> Share = '1/3 * T' ; Share = '1/3').
+
+/* has children */
+inherit_from(Deceased, Mother, Share):-
+    person(Mother, female, alive),
+    person(Father, male, _),
+    spouse(Father, Mother),
+    deceased_has_children(Deceased, true),
+    Share = '1/6'.
+
+
+
